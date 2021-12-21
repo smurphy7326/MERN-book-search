144a345695626014bc651aa47588fdd6bf587c00
smurphy7326 <smurphy7326@gmail.com>
2021-12-21 14:04:52 -0500

    working on installing apollo client and mutations need more work done on it

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client'
+
+// login is standard like in the weekly module that we used
+export const LOGIN_USER = gql`
+    mutation Login($email: String!, $password: String!){
+        login(email: $email, password: $password){
+        token
+        user{
+            _id
+        }
+    }
+}
+`;
+
+// add user is the same as the weekly module again 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql `
+
+    {
+        me{
+        _id
+        username
+        email
+        savedBooks{
+            bookId
+            title
+            authors
+            description
+            image
+            link
+            title
+        }    
+      }
+    }
+;`
